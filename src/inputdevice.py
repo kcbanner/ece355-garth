cf8087b9d06ffe24c2915827026e92d7a9429cc5
Ben Ridder <ben@ridder.ca>
2012-03-14 20:08:00 -0400

    input devices

---
+++ src/inputdevice.py
@@ -0,0 +1,32 @@
+from event import *
+from event_type import EventType
+
+class InputDevice():
+    def __init__(self, device_id):
+        self.device_id = device_id
+
+    def get_device_id(self):
+        return self.device_id
+
+class NFCReaderInputDevice(InputDevice):
+    def __init__(self, device_id):
+        InputDevice.__init__(self, device_id)
+        self.data = None
+    
+    def set_data(self, data):
+        self.data = data
+        
+    def generate_NFC_event(self):
+        return NFCEvent(EventType.NFC_EVENT, self.device_id, self.data)
+
+class KeypadInputDevice(InputDevice):
+    def __init__(self, device_id):
+        InputDevice.__init__(self, device_id)
+        self.input_char = None
+
+    def set_input_char(self, char):
+        self.input_char = char
+
+    def generate_keypad_event(self):
+        return KeypadEvent(EventType.KEYPAD_EVENT, self.device_id,
+                            self.input_char)
