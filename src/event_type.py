c8c9877eb83bdf5d3a4489195bd34b898d68538e
Ben Ridder <ben@ridder.ca>
2012-03-17 21:18:36 -0400

    did window sensor handler

--- src/event_type.py
+++ src/event_type.py
@@ -3,7 +3,7 @@ class EventType:
     WINDOW_SENSOR_EVENT = 2
     FLOOD_SENSOR_EVENT  = 3
     TEMP_SENSOR_EVENT   = 4
-    ALARM_SENSOR_EVENT  = 5
+    ALARM_EVENT         = 5
     KEYPAD_EVENT        = 6
     NFC_EVENT           = 7
     MOTION_SENSOR_EVENT = 8
